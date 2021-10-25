65ea755695a707abab787b2425110771ee2f7fec
Thomas D. Pellegrin <contact@theaviationdoctor.com>
2021-10-26 00:19:36 +0800

    Progressed 6_takeoff.R

--- scripts/6_takeoff.R
+++ scripts/6_takeoff.R
@@ -17,6 +17,7 @@ library(data.table)
 library(DBI)
 library(ggplot2)
 library(parallel)
+library(zoo)
 
 # Import the common settings
 source("scripts/0_common.R")
@@ -66,74 +67,9 @@ cat("\014")
   
   # Load the aircraft and engine data from a CSV file
   dt_aer <- fread(file = paste(path_aer, aer_act, sep = "/"), header = TRUE, colClasses = c("character", "factor", "integer", "integer", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
-  
+
 ################################################################################
-# Define simulation functions                                                  #
-################################################################################
-  
-  ##############################################################################
-  # Define a function to convert degrees Kelvin to Celsius                     #
-  ##############################################################################
-  
-  fn_k_to_c <- function(k) {
-    k - 273.15
-  }
-  
-  ##############################################################################
-  # Define a function to convert meters per second to knots (intl. naut. mph.) #
-  ##############################################################################
-  
-  fn_ms_to_kt <- function(ms) {
-    ms * 1.9438445
-  }
-  
-  ##############################################################################
-  # Define a function to convert knots to meters per second                    #
-  ##############################################################################
-  
-  fn_kt_to_ms <- function(kt) {
-    kt * 0.5144442
-  }
-  
-  ##############################################################################
-  # Define a function to calculate the maximum takeoff thrust                  #
-  # CT1, CT2, and CT3 are coefficients of max thrust as per BADA3              #
-  # HP is the geopotential pressure altitude                                   #
-  # As per the method used in Gratton et al. (2020) & proposed by Nuic (2013)  #
-  ##############################################################################
-  
-  fn_thrust <- function(act, Hp) {
-    dt_aer[aircraft == act, CT1] * ( 1 - ( Hp / dt_aer[aircraft == act, CT2] ) + ( dt_aer[aircraft == act, CT3] * Hp^2 ) )
-  }
-  
-  ##############################################################################
-  # Define a function to calculate the lift force in newtons for a given speed #
-  ##############################################################################
-  
-  fn_lift <- function(act, rho, Vtas) {
-    .5 * rho * Vtas^2 * dt_aer[aircraft == act, S] * dt_aer[aircraft == act, CL]
-  }
-  
-  ##############################################################################
-  # Define a function to calculate the drag force in newtons for a given speed #
-  ##############################################################################
-  
-  fn_drag <- function(act, rho, Vtas) {
-    .5 * rho * Vtas^2 * dt_aer[aircraft == act, S] * dt_aer[aircraft == act, CD]
-  }
-  
-  ##############################################################################
-  # Define a function to approximate the minimum takeoff speed                 #
-  # V2min is the minimum takeoff safety speed in takeoff configuration         #
-  # Vs is the stall speed in takeoff configuration                             #
-  ##############################################################################
-  
-  fn_speed <- function(act) {
-    dt_aer[aircraft == act, Vs] * 1.2
-  }
-  
-################################################################################
-# Define a function to simulate takeoffs                                       #
+# Define a function to simulate takeoffs at each airport                       #
 ################################################################################
   
 fn_takeoff <- function(apt) {
@@ -146,7 +82,7 @@ fn_takeoff <- function(apt) {
   db_con <- dbConnect(RMySQL::MySQL(), default.file = db_cnf, group = db_grp)
   
   # Build the query to retrieve the takeoff conditions
-  db_qry <- paste("SELECT id, obs, exp, tas, rho, wnd_hdw, rwy, toda FROM ", db_cli, " WHERE icao = '", apt, "' LIMIT 1;", sep = "")
+  db_qry <- paste("SELECT id, obs, exp, tas, ps, rho, wnd_hdw, rwy, toda FROM ", db_cli, " WHERE icao = '", apt, "' LIMIT 1;", sep = "")
   
   # Send the query to the database
   db_res <- suppressWarnings(dbSendQuery(db_con, db_qry))
@@ -161,67 +97,84 @@ fn_takeoff <- function(apt) {
   dbDisconnect(db_con)
   
   ##############################################################################
-  # For each climate observation at the airport                                #
+  # Define airport-level simulation inputs                                     #
+  ##############################################################################
+  
+  # Natural inputs
+  g <- 9.806665 # Gravitational acceleration in meters per second squared, assuming a non-oblate, non-rotating Earth (Blake, 2009; Daidzic, 2016)
+  
+  # Aircraft inputs
+  act <- "A320"                         # Aircraft model
+  CL  <- dt_aer[aircraft == act, CL]    # Dimensionless coefficient of lift in takeoff (non-clean) configuration
+  CD  <- dt_aer[aircraft == act, CD]    # Dimensionless coefficient of drag in takeoff (non-clean) configuration
+  # m   <- dt_aer[aircraft == act, MTOW]  # Aircraft mass in kilograms
+  m   <- 68000  # Aircraft mass in kilograms
+  S   <- dt_aer[aircraft == act, S]     # Wing surface area in square meters
+  W   <- m * g                          # Aircraft weight in newtons
+  
+  # Engine inputs
+  Tmax <- dt_aer[aircraft == act, Tmax] # Maximum sea-level static thrust under ISA conditions
+  CT1  <- dt_aer[aircraft == act, CT1]  # First coefficient of maximum takeoff thrust
+  CT2  <- dt_aer[aircraft == act, CT2]  # Second coefficient of maximum takeoff thrust
+  CT3  <- dt_aer[aircraft == act, CT2]  # Third coefficient of maximum takeoff thrust
+  
+  # Airport inputs
+  mu <- mu["blake"]                     # Coefficient of rolling friction
+  
+  # Simulation settings
+  int <- 30                             # Resolution / number of integration steps (i.e., number of speed increments between zero and minimum takeoff speed)
+  
+  ##############################################################################
+  # Define airport-level simulation outputs                                    #
+  ##############################################################################
+  
+  # Initialize a data table to hold the results for the current airport
+  dt_apt <- data.table()
+  
+  ##############################################################################
+  # For each climate observation at the current airport                        #
   ##############################################################################
   
   for (i in 1:nrow(dt_smp)) {
     
     ############################################################################
-    # Define simulation inputs                                                 #
+    # Define observation-level simulation inputs                               #
+    ############################################################################
+      
+    # Climate inputs
+    exp <- dt_smp[i, exp]          # Climate experiment (SSP)
+    hdw <- dt_smp[i, wnd_hdw]      # Headwind in meters per second at the active runway
+    oat <- dt_smp[i, tas] - 273.15 # Outside air temperature converted from kelvins to degrees Celsius
+    obs <- dt_smp[i, obs]          # Date and time
+    rho <- dt_smp[i, rho]          # Air density in kilograms per cubic meter
+    ps  <- dt_smp[i, ps]           # Air pressure in pascals
+    
+    # Runway inputs
+    rwy   <- dt_smp[i, rwy]        # Active runway (based on the prevailing wind at the time of the observation)
+    theta <- 0                     # Runway slope (theta) in degrees
+
+    ############################################################################
+    # Calculate the speed at which lift L is equal to weight W                 #
     ############################################################################
     
-      ##########################################################################
-      # Aircraft inputs                                                        #
-      ##########################################################################
-      
-      act <- "A320"                          # Aircraft model
-      int <- 10                              # Set integration steps (i.e., number of speed increments between zero and minimum takeoff speed)
-      
-      ##########################################################################
-      # Climate inputs                                                         #
-      ##########################################################################
-      
-      exp <- dt_smp[i, exp]                  # Climate experiment (SSP)
-      hdw <- fn_ms_to_kt(dt_smp[i, wnd_hdw]) # Headwind converted to knots
-      oat <- fn_k_to_c(dt_smp[i, tas])       # Outside air temperature converted to degrees Celsius
-      obs <- dt_smp[i, obs]                  # Date and time
-      rho <- dt_smp[i, rho]                  # Air density in kilograms per cubic meter
-      
-      ##########################################################################
-      # Runway inputs                                                          #
-      ##########################################################################
-      
-      mu  <- mu["blake"]                     # Coefficient of rolling friction
-      rwy <- dt_smp[i, rwy]                  # Active runway (based on the prevailing wind at the time of the observation)
-      slp <- 0                               # Runway slope (theta) in degrees
-      
-    ############################################################################
-    # Define groundspeed and airspeed increments for the step integration      #
-    ############################################################################
-    
-    # Approximate the minimum takeoff true airspeed in takeoff configuration
-    V2min <- fn_speed(act)
-    
-    # Define groundspeed increments in knots from zero to the minimum takeoff true airspeed in takeoff configuration
-    # If there is headwind, the maximum groundspeed we need to achieve is decreased
+    # Calculate minimum takeoff speed and add a 1.2 safety margin factor
+    V2min <- sqrt( W / (.5 * S * rho * CL) ) * 1.2
+
+    # Define groundspeed increments. Headwind decreases the terminal groundspeed
     Vgnd <- seq(from = 0, to = V2min - hdw, length = int)
-    
-    # Define airspeed increments in knots from zero to the minimum takeoff true airspeed in takeoff configuration
-    # If there is headwind, the minimum airspeed when the aircraft is idle is nonzero
+
+    # Define airspeed increments. Headwind increases the starting airspeed
     Vtas <- seq(from = 0 + hdw, to = V2min, length = int)
     
     ############################################################################
-    # Simulate the aerodynamic forces (lift and drag)                          #
+    # Simulate the aerodynamic forces (lift and drag) at every V               #
     ############################################################################
     
-    # Calculate the lift force in newtons at each true airspeed increment
-    L <- fn_lift(act, rho, fn_kt_to_ms(Vtas))
+    # Calculate the lift force in newtons at each airspeed increment
+    L <- .5 * rho * Vtas^2 * S * CL
     
-    # Calculate the drag force in newtons at each true airspeed increment
-    D <- fn_drag(act, rho, fn_kt_to_ms(Vtas))
-    
-    # Calculate the dynamic pressure at each true airspeed increment
-    q <- (rho * fn_kt_to_ms(Vtas)^2) / 2
+    # Calculate the drag force in newtons at each airspeed increment
+    D <- .5 * rho * Vtas^2 * S * CD
     
     ############################################################################
     # Simulate the propulsive force (thrust) at every V                        #
@@ -230,92 +183,81 @@ fn_takeoff <- function(apt) {
     # NEED TO CALCULATE THE AIRPORT'S GEOPOTENTIAL PRESSURE ALTITUDE HERE
     Hp <- 0
     
-    # Calculate maximum possible thrust (i.e., TOGA thrust) at the start of the takeoff
-    Tmax <- fn_thrust(act, Hp)
-    
-    # Simulate reduced takeoff operations (takeoff at no less than 75% of TOGA thrust)
-    Trto <- Tmax * .75
+    # Set reduced takeoff operations (takeoff at no less than 75% of TOGA thrust)
+    Trto <- Tmax  * .75
     
     ############################################################################
-    # Simulate the gravitational force (weight)                                #
+    # Simulate the acceleration of the aircraft along the runway               #
     ############################################################################
     
-    # # Gravitational acceleration in meters per second squared, assuming a non-oblate, non-rotating Earth (Blake, 2009; Daidzic, 2016)
-    # g <- 9.806665
-    # 
-    # # Aircraft takeoff weight, expressed in kilograms rather than newtons for convenience
-    # weight <- W[act]
-    # 
-    # # Aircraft takeoff weight in newtons
-    # weight <- g * mass
-    # 
-    # # Mu times weight
-    muW <- mu * dt_aer[aircraft == act, MTOW]
+    # Calculate the dynamic pressure at each airspeed increment
+    q <- (rho * Vtas^2) / 2
+    
+    # Acceleration in meters per second squared
+    a <- g / W * (Trto * 2 - (mu * W) - (CD - (mu * CL)) * (q * S) - (W * sin(theta)))
+    
+    ############################################################################
+    # Step integration of distance as per Table 18-2 of Blake (2019)           #
+    ############################################################################
+    
+    # Calculate the average acceleration (i.e., midpoint acceleration)
+    a_avg <- frollmean(x = a, n = 2, fill = head(a, 1), align = "right")
+    
+    # Calculate the average speed between two speed increments
+    Vgnd_avg <- frollmean(x = Vgnd, n = 2, fill = 0, align = "right")
+    
+    # Calculate the difference between two speed increments
+    Vgnd_inc <- Vgnd - shift(x = Vgnd, n = 1, fill = 0, type = "lag")
+    
+    # Calculate the distance covered in each increment
+    dis <- Vgnd_avg * Vgnd_inc / a_avg
+    
+    # Increment the cumulative (total) distance by that incremental distance
+    cum <- cumsum(x = dis)
     
     ############################################################################
     # Return results                                                           #
     ############################################################################
     
-    # Assemble a data frame and display it in the log file
-    out <- data.frame("ICAO" = apt, "Observation" = obs, "Experiment" = exp, "Runway" = rwy, "Aircraft" = act, "Groundspeed" = round(Vgnd, digits = 2), "Airspeed" = round(Vtas, digits = 2), "DynamicPressure" = round(q, digits = 2), "Thrust" = round(Trto, digits = 2), "Slope" = slp, "muW" = round(muW, digits = 2), "Lift" = round(L, digits = 2), "Weight" = dt_aer[aircraft == act, MTOW], row.names = NULL)
-    width <- 200
+    # Assemble the results into a data table
+    dt_obs <- data.table(
+      icao     = rep(apt, int),
+      obs      = rep(obs, int),
+      exp      = rep(exp, int),
+      rwy      = rep(rwy, int),
+      hdw      = rep(hdw, int),
+      ps       = rep(ps, int),
+      oat      = rep(oat, int),
+      act      = rep(act, int),
+      Tmax     = rep(Tmax, int),
+      Trto     = rep(Trto, int),
+      CL       = rep(CL, int),
+      CD       = rep(CD, int),
+      V2min    = rep(V2min, int),
+      Vgnd     = Vgnd,
+      Vtas     = Vtas,
+      L        = L,
+      W        = W,
+      D        = D,
+      q        = q,
+      a        = a,
+      a_avg    = a_avg,
+      Vgnd_avg = Vgnd_avg,
+      Vgnd_inc = Vgnd_inc,
+      dis      = dis,
+      cum      = cum
+    )
+    
+    # Bind the per-observation results to the per-airport data table
+    
+    # Display it in the log file
+    width <- 240
     options(width = width)
     print(paste(rep("#", width), collapse = ""))
-    print(out)
+    print(dt_obs)
     print(paste(rep("#", width), collapse = ""))
     
   } # End of the loop
-    
-    
-    
-  # # Calculate the thrust value for each V
-  # for (j in 2:length(V)){
-  #   T[j] <- T[j-1] + (V[j] - V[j-1]) * coef(model)[2]
-  # }
-  # 
-  # # Dynamic pressure. The speed is converted to feet per second
-  # q <- (dt_smp$rho[i] * (V * 1.68781)^2) / 2
-  # 
-  # 
-  # g <- 32.174
-  # mu <- .0165
-  # CL <- 1.9805065
-  # S <- 1951
-  # 
-  # # Acceleration in feet per second per second
-  # a <- g / W * (T * 2 - (mu * W) - (CD - (mu * CL)) * (q * S) - (W * sin(slp)))
-  # 
-  # print(paste("GS:", (GS), sep = " "))
-  # print(paste("V:", (V), sep = " "))
-  # print(paste("T:", (T), sep = " "))
-  # print(paste("q:", (q), sep = " "))
-  # print(paste("a:", (a), sep = " "))
-  #   
-  # # For each groundspeed increment
-  # for (j in 1:length(GS)) {
-  # 
-  #   # Calculate the average speed (i.e., midpoint speed), skipping the zero speed row
-  #   avg <- ifelse(GS[j] == 0, GS[j], mean(c(GS[j - 1], GS[i])))
-  # 
-  #   # Calculate the speed increment (i.e., new speed minus previous speed), skipping the zero speed row
-  #   inc <- ifelse(GS[j] == 0, GS[j], GS[j] - GS[j - 1])
-  # 
-  #   # Calculate the average acceleration (i.e., midpoint acceleration)
-  #   acc <- mean(c(a[j - 1], a[j]))
-  # 
-  #   # Calculate the distance covered in that increment (converting speed from knots to feet/s)
-  #   dis <- avg * inc * 1.6878^2 / acc
-  # 
-  #   # Increment the cumulative (total) distance by that incremental distance
-  #   cum <- cum + dis
-  # 
-  #   # Save incremental results for later display to the console
-  #   out1 <- rbind(out1, c(GS[j], V[j], round(a[j], digits = 1), round(dis, digits = 0), round(cum, digits = 0)))
-  # 
-  # }
-  # 
-  # # Output results to the console
-  # print(out1)
   
 } # End of the fn_takeoff function
 
@@ -342,7 +284,7 @@ clusterEvalQ(cl, {
 })
 
 # Pass the required variables from the main scope to the workers' scope
-clusterExport(cl, c("db_cnf", "db_grp", "db_imp", "db_cli", "dt_aer", "fn_drag", "fn_lift", "fn_k_to_c", "fn_kt_to_ms", "fn_ms_to_kt", "fn_speed", "fn_thrust", "mu"))
+clusterExport(cl, c("db_cnf", "db_grp", "db_imp", "db_cli", "dt_aer", "mu"))
 
 # LIMIT NUMBER OF AIRPORTS FOR TESTING ONLY - REMOVE BEFORE GO-LIVE
 df_smp <- head(df_smp, 1)
