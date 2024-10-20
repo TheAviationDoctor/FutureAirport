25aaf44bbc3c802363db359797e41eedd86c293f
Thomas D. Pellegrin <47185363+TheAviationDoctor@users.noreply.github.com>
2024-10-20 22:16:51 +0800

    Progressed app.R

--- app.R
+++ app.R
@@ -20,24 +20,66 @@ library(bsicons)
 library(bslib)
 library(data.table)
 library(dplyr)
+library(htmlwidgets)
 library(leaflet)
 library(shiny)
 library(shinyBS)
 library(shinyjs)
 
+# Define a Bootstrap theme using bslib
+# theme = bs_theme(version = 5, bootswatch = "cerulean")  # Example of a custom theme
+
 # Clear the console
 cat("\014")
+
+# Load the airport data
+dt_apt <- fread(
+  file          = "data/apt/airports.csv",
+  header        = TRUE,
+  colClasses    = c(rep("character", 3L), rep("numeric", 2L), "factor")
+) |>
+  setkey(cols   = iata)
+
+# Load the climate data
+dt_cli <- fread(
+  file          = "data/cli/cli.csv",
+  header        = TRUE,
+  colClasses    = c(rep("factor", 3L), rep("numeric", 12L))
+) |>
+  setkey(cols   = icao, ssp, year)
+
+# Define list items for the selectors
+choices <- list(
+  "apt" = dt_apt$icao,          # Airports
+  "ssp" = unique(dt_cli$ssp),   # SSPs
+  "sta" = colnames(dt_cli)[4:9] # Statistic
+)
+
+# Define display names for the list items
+# Airports
+names(choices$apt) <- paste(dt_apt$iata, "/", dt_apt$icao, " ", dt_apt$name, sep = "")
+# SSPs
+names(choices$ssp) <- c(
+  paste(toupper(substring(choices$ssp[1], 0, 4)), "Sustainability (best-case scenario)",             sep = " - "),
+  paste(toupper(substring(choices$ssp[2], 0, 4)), "Middle of the road",                              sep = " - "),
+  paste(toupper(substring(choices$ssp[3], 0, 4)), "Regional rivalry",                                sep = " - "),
+  paste(toupper(substring(choices$ssp[4], 0, 4)), "Fossil-fueled development (worst-case scenario)", sep = " - ")
+)
+# Statistics
+names(choices$sta) <- c(
+  "Minimum", "1st quartile", "Mean", "Median", "3rd quartile", "Maximum"
+)
 
 # ==============================================================================
 # 1 UI layout
 # ==============================================================================
 
-# ui <- fluidPage(
 ui <- fillPage(
-  useShinyjs(),  # Enable shinyjs if needed for any further interactivity
-  # Define a Bootstrap theme using bslib
-  theme = bs_theme(version = 5, bootswatch = "cerulean"),  # Example of a custom theme
-  # CSS
+
+  # ==============================================================================
+  # 1.1 CSS
+  # ==============================================================================
+
   tags$head(
     tags$style(
       HTML("
@@ -63,15 +105,27 @@ ui <- fillPage(
       ")
     )
   ),
-  # Map
+
+  # ==============================================================================
+  # 1.2 Display map
+  # ==============================================================================
+
   leafletOutput("map", width = "100%", height = "100%"),
-  # Controls
+
+  # ==============================================================================
+  # 1.3 Display controls
+  # ==============================================================================
+
   div(
     id = "controls",
-    h4("Surface air temperature at airports worldwide, 2015–2100"),
+
+    # Title
+    h3("Surface air temperature at airports worldwide, 2015–2100"),
+
+    # Airport selector
     span("Select an airport:"),
     tooltip(
-      span(bsicons::bs_icon("info-circle-fill")),
+      span(bs_icon("info-circle-fill")),
       "Optionally, pick one of the ~900 airports worldwide with at least 1M passengers in annual traffic, sorted alphabetically by their IATA code. 'All' will display all airports at once.",
       placement = "bottom"
     ),
@@ -79,25 +133,48 @@ ui <- fillPage(
       inputId  = "airport",
       label    = NULL,
       choices  = NULL,
-      selected = NULL,
       width    = "100%"
     ),
+
+    # SSP selector
     span("Select a climate scenario:"),
     tooltip(
-      span(bsicons::bs_icon("info-circle-fill")),
+      span(bs_icon("info-circle-fill")),
       "Shared Socioeconomic Pathways (SSPs) are climate change scenarios defined by the Intergovernmental Panel on Climate Change (IPCC) to standardize climate research. They are based on projected socioeconomic development trajectories up to the year 2100. The IPCC Sixth Report (2021) described SSP2 as likely, hence it is selected as default here.",
       placement = "bottom"
     ),
     selectInput(
       inputId  = "ssp",
       label    = NULL,
-      choices  = names(ssp_choices),
+      choices  = NULL,
       width    = "100%"
     ),
-    # htmlOutput("ssp_desc"),
+
+    # Climate statistic selector
+    span("Select which statistic to display:"),
+    tooltip(
+      span(bs_icon("info-circle-fill")),
+      "By default, the map displays changes in the mean (average) annual surface air temperature at the selected airport(s) relative to the 2015 baseline. However, you can also choose to display the annual minimum, first quartile (25th percentile), median (50th percentile), third quartile (75th percentile), or maximum instead. It is possible, for example, that the temperature maxima increase after than the mean over time at some locations.",
+      placement = "bottom"
+    ),
+    radioButtons(
+      inputId  = "sta",
+      label    = NULL,
+      choices  = choices$sta,
+      inline   = TRUE,
+      width    = "100%"
+    ),
+
+    # Year selector
+    span("Select an observation year:"),
+    tooltip(
+      span(bs_icon("info-circle-fill")),
+      "2015 is taken as the baseline year for all subsequent observations, which are expressed in degrees Celsius above or below that baseline.",
+      placement = "bottom"
+    ),
     sliderInput(
       inputId  = "year",
-      label    = "Select an observation year:",
+      label    = NULL,
       min      = 2015,
       max      = 2100,
       value    = 2015,
@@ -105,6 +182,13 @@ ui <- fillPage(
       sep      = "",
       width    = "100%"
     ),
+
+    # For debugging only
+    htmlOutput("airport"),
+    htmlOutput("ssp"),
+    htmlOutput("sta"),
+    htmlOutput("year"),
+    htmlOutput("dat")
   )
 )
 
@@ -114,106 +198,102 @@ ui <- fillPage(
 
 server <- function(input, output, session) {
 
-  # Load the airport data
-  dt_apt <- fread(
-    file          = "data/apt/airports.csv",
-    header        = TRUE,
-    colClasses    = c(rep("character", 3L), rep("numeric", 2L), "factor")
-  ) |>
-    setkey(cols   = iata)
+  # ==============================================================================
+  # 2.1 Update selectors dynamically
+  # ==============================================================================
 
-  # Cosmetic names for the dropdown
-  apt_choices <- dt_apt$icao
-  names(apt_choices) <- paste(dt_apt$iata, "/", dt_apt$icao, " ", dt_apt$name, sep = "")
-
-  # Update airport dropdown choices dynamically
+  # Airport selector
   observe(
     {
       updateSelectInput(
         session,
         inputId = "airport",
         label   = NULL,
-        choices = c("All", apt_choices)
+        choices = c("All", choices$apt)
       )
     }
   )
 
-  # Load the climate data
-  dt_cli <- fread(
-    file          = "data/cli/cli.csv",
-    header        = TRUE,
-    colClasses    = c(rep("factor", 3L), rep("numeric", 12L))
-  ) |>
-    setkey(cols   = icao, var, ssp)
-
-  # Cosmetic names for the ssp dropdown
-  ssp_choices <- toupper(unique(dt_cli$ssp))
-  names(ssp_choices) <- c(
-    "SSP1 — Sustainability (best-case scenario)",
-    "SSP2 — Middle of the road",
-    "SSP3 — Regional rivalry",
-    "SSP5 — Fossil-fueled development (worst-case scenario)"
-  )
-
-  # Update ssp dropdown choices dynamically
+  # SSP selector
   observe(
     {
       updateSelectInput(
         session,
         inputId = "ssp",
         label   = NULL,
-        choices = ssp_choices
+        choices = choices$ssp,
+        selected = choices$ssp[2]
       )
     }
   )
 
-  # Update the SSP description
-  ssp_desc <- reactive(
+  # Climate statistic selector
+  observe(
     {
-      case_when(
-        input$ssp == "SSP126" ~ "The world shifts gradually, but pervasively, toward a more sustainable path, emphasizing more inclusive development that respects perceived environmental boundaries. Management of the global commons slowly improves, educational and health investments accelerate the demographic transition, and the emphasis on economic growth shifts toward a broader emphasis on human well-being. Driven by an increasing commitment to achieving development goals, inequality is reduced both across and within countries. Consumption is oriented toward low material growth and lower resource and energy intensity.",
-        input$ssp == "SSP245" ~ "The world follows a path in which social, economic, and technological trends do not shift markedly from historical patterns. Development and income growth proceeds unevenly, with some countries making relatively good progress while others fall short of expectations. Global and national institutions work toward but make slow progress in achieving sustainable development goals. Environmental systems experience degradation, although there are some improvements and overall the intensity of resource and energy use declines. Global population growth is moderate and levels off in the second half of the century. Income inequality persists or improves only slowly and challenges to reducing vulnerability to societal and environmental changes remain.",
-        input$ssp == "SSP370" ~ "A resurgent nationalism, concerns about competitiveness and security, and regional conflicts push countries to increasingly focus on domestic or, at most, regional issues. Policies shift over time to become increasingly oriented toward national and regional security issues. Countries focus on achieving energy and food security goals within their own regions at the expense of broader-based development. Investments in education and technological development decline. Economic development is slow, consumption is material-intensive, and inequalities persist or worsen over time. Population growth is low in industrialized and high in developing countries. A low international priority for addressing environmental concerns leads to strong environmental degradation in some regions.",
-        input$ssp == "SSP585" ~ "This world places increasing faith in competitive markets, innovation and participatory societies to produce rapid technological progress and development of human capital as the path to sustainable development. Global markets are increasingly integrated. There are also strong investments in health, education, and institutions to enhance human and social capital. At the same time, the push for economic and social development is coupled with the exploitation of abundant fossil fuel resources and the adoption of resource and energy intensive lifestyles around the world. All these factors lead to rapid growth of the global economy, while global population peaks and declines in the twenty-first century. Local environmental problems like air pollution are successfully managed. There is faith in the ability to effectively manage social and ecological systems, including by geo-engineering if necessary."
+      updateRadioButtons(
+        session,
+        inputId  = "sta",
+        label    = NULL,
+        choices  = choices$sta,
+        selected = choices$sta[3],
+        inline   = TRUE
       )
     }
   )
 
-  # Render the SSP description
-  output$ssp_desc <- renderText(ssp_desc())
+  # ==============================================================================
+  # 2.2 Filter climate data dynamically based on selector inputs
+  # ==============================================================================
 
-  # Filter data based on the slider and dropdown inputs
   filtered_data <- reactive(
     {
-      data <- dt_cli |>
-        filter(year == as.character(input$year))
-      if (input$airport != "All") {
-        data <- data |>
-          filter(icao == input$airport)
-      }
-      return(data)
+      
+      # # WORKING START
+      # # Filter by year
+      # data <- dt_cli |> filter(year == as.character(input$year))
+      # 
+      # # Filter by airport
+      # if (input$airport != "All") { data <- data |> filter(icao == input$airport) }
+      # 
+      # return(data)
+      # # WORKING END
+
+      return(
+        dt_cli |>
+          # Filter by airport
+          filter(if (input$airport == "All") icao != input$airport else icao == input$airport) |>
+          # Filter by SSP
+          filter(ssp == input$ssp) |> 
+          # Filter by year
+          filter(year == as.character(input$year)) |>
+          # Select only data columns for the chosen climate statistic
+          select(icao, any_of(c(input$sta, paste(input$sta, "dif", sep = "_"))))
+      )
+
     }
   )
+
+  # ==============================================================================
+  # 2.3 Render and update the map
+  # ==============================================================================
 
   # Render the map
   output$map <- renderLeaflet(
     {
       leaflet(options = leafletOptions(zoomControl = FALSE)) |>
         addProviderTiles("OpenStreetMap") |>
-        setView(lng = 0, lat = 10, zoom = 3) |>
-        htmlwidgets::onRender(
-          "function(el, x) {
-          L.control.zoom({position:'topright'}).addTo(this);
-        }")
+        setView(lng = 0, lat = 50, zoom = 3) |>
+        # Move zoon controls to top right
+        onRender("function(el, x) { L.control.zoom({position:'topright'}).addTo(this); }")
     }
   )
-  
-  # Observe filtered data and update the map
+
+  # Update the map
   observe(
     {
       data <- filtered_data()
       # Create color palette for temperature
-      pal <- colorNumeric(palette = "RdYlBu", domain = data$tas, reverse = TRUE)
+      pal <- colorNumeric(palette = "RdYlBu", domain = c(0, 2), reverse = TRUE)
       # Add points to the map
       leafletProxy("map", data = data) |>
         clearMarkers() |> 
@@ -221,13 +301,20 @@ server <- function(input, output, session) {
           lng         = ~dt_apt[icao == icao, lon],
           lat         = ~dt_apt[icao == icao, lat],
           radius      = 5,
-          color       = ~pal(mean),
+          # color       = ~pal(mean_dif),
+          # color       = ~pal(mean_dif),
           stroke      = FALSE,
           fillOpacity = 0.8,
           popup       = ~paste("ICAO:", icao, "<br>", "Name:", icao)
         )
     }
   )
+  
+  # For debugging only
+  output$airport <- renderText(input$airport)
+  output$ssp     <- renderText(input$ssp)
+  output$sta     <- renderText(input$sta)
+  output$dat     <- renderText(unlist(filtered_data()))
 }
 
 # Run the app
